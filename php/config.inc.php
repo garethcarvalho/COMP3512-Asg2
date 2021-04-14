519406c3a6d409e309b2aff2ac7d3ca0e6ceada8
garethcarvalho <46726440+garethcarvalho@users.noreply.github.com>
2021-04-14 11:02:59 -0600

    Update config.inc.php

--- php/config.inc.php
+++ php/config.inc.php
@@ -1,8 +1,8 @@
 <?php
-define('DBHOST', 'localhost');
+define('DBHOST', '%');
 define('DBNAME', 'stocks');
-define('DBUSER', 'root');
-define('DBPASS', '');
+define('DBUSER', 'student');
+define('DBPASS', '123');
 define('DBCONNSTRING',"mysql:host=" . DBHOST . ";dbname=" . DBNAME . ";charset=utf8mb4;");
 $pdo = new PDO(DBCONNSTRING,DBUSER,DBPASS); 
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
