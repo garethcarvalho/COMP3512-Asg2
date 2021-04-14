8d618824c9af4cb323405f7e22172585243adba2
garethcarvalho <46726440+garethcarvalho@users.noreply.github.com>
2021-04-14 11:05:52 -0600

    Update list.php

--- list.php
+++ list.php
@@ -41,7 +41,7 @@
 
     <!-- <div id="list-container" style="overflow: none;"> -->
     <!-- style="height: 700px; overflow-y: scroll" -->
-        <div id="zoom-div"><img id="zoom-image" src="http://localhost/stock/img/logos/A.svg"></div>
+        <div id="zoom-div"><img id="zoom-image" src="./img/logos/A.svg"></div>
         <div id="loading" style="text-align:center;width:100px;height:100px;"><img style="width:80px;height: 80px;object-fit:cover;text-align:center;" src="img/loading.gif"></div>
         <table id="list-table"  style="height: 720px; overflow-y: scroll; margin: auto;">
             <thead>
