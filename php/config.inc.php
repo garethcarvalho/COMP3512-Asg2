8507e0801d621920cb681abc6ca0664e1d418495
garethcarvalho <46726440+garethcarvalho@users.noreply.github.com>
2021-04-14 16:51:38 -0600

    Update config.inc.php

--- php/config.inc.php
+++ php/config.inc.php
@@ -3,7 +3,8 @@ define('DBHOST', 'localhost');
 define('DBNAME', 'stocks');
 define('DBUSER', 'root');
 define('DBPASS', '');
-define('DBCONNSTRING',"mysql:host=" . DBHOST . ";dbname=" . DBNAME);
+define('DBCONNSTRING', 'sqlite:./database/sqlite/stocks.db');
+// define('DBCONNSTRING',"mysql:host=" . DBHOST . ";dbname=" . DBNAME);
 $pdo = new PDO(DBCONNSTRING,DBUSER,DBPASS); 
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
